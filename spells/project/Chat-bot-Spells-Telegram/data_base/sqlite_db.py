d26180e9595855769c0e4899836c76b1c8ac757c
stacybat <stacy.bat@googlemail.com>
2022-06-09 21:20:04 +0300

    Added search for spell bases

--- spells/project/Chat-bot-Spells-Telegram/data_base/sqlite_db.py
+++ spells/project/Chat-bot-Spells-Telegram/data_base/sqlite_db.py
@@ -11,7 +11,8 @@ def sql_start():
     if base:
         print('Data base connected')
 
-async def sql_select_command(message, search_text):
+
+async def sql_select_spell(message, search_text):
     if len(search_text) > 0:
         request = "SELECT * FROM spellindexbytechnique WHERE NAME LIKE \'%%%s%%\'"%(search_text)
         print(request)
@@ -21,7 +22,26 @@ async def sql_select_command(message, search_text):
             for i in r:
                 if len(str(i)) >0: r_str += ' ' + str(i)
             await message.reply(r_str) 
-            
-            #result.append(r_str)
-    #print(*result, sep = '\n')
-    #return result
+
+
+async def sql_select_base(message, search_base):
+    level = '0'
+    tech = ''
+    form = ''
+    for i in search_base.split():
+        if i.capitalize() in ['Cr', 'In', 'Mu', 'Pe', 'Re']:
+            tech = i.capitalize()
+        elif i.capitalize() in ['An', 'Aq', 'Au', 'Co', 'He', 'Ig', 'Im', 'Me', 'Te', 'Vi']:
+            form = i.capitalize()
+        elif i.isdigit():
+            level = i
+    if tech !='' and form !='':
+        request = "SELECT Base FROM TechForm WHERE Tech = \'%s\' and Form = \'%s\' and level = %s"%(tech, form, level)
+        print(request)
+        for r in cur.execute(request).fetchall():
+            r_str=''
+            for i in r:
+                if len(str(i)) >0: r_str += ' ' + str(i)
+            await message.reply(r_str) 
+
+
--- spells/project/Chat-bot-Spells-Telegram/handlers/commands_handler.py
+++ spells/project/Chat-bot-Spells-Telegram/handlers/commands_handler.py
@@ -1,56 +1,31 @@
 from aiogram import types, Dispatcher
 from create_bot import dp
 from data_base import sqlite_db
-#from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-#class FSMqueery_search(StatesGroup):
-#    q_search = State()
-#    result = State()
 
+async def command_start(message: types.Message):  
+    await message.reply('Hello! This bot is looking for pages where the spell is described in the books of Ars Magica and looking for spell bases')
 
-#@dp.message_handler(commands=['start', 'help'])
-async def command_start(message: types.Message):
-    #await FSMqueery_search.q_result.set()
-    await message.reply('Hello! If you want to search for a spell by a keyword, enter /f *search_word*')
-    print('1')
-    #await FSMqueery_search.q_search.set()
-    print('2')
     
+async def command_help(message: types.Message):
+    await message.reply('If you want to search a spell, enter /fs *search_word*\n If you want to search for a base, enter /fb Tech. Form Level')
 
-
-#@dp.message_handler()
-#async def search_command(message : types.Message):
-    #await FSMqueery_search.q_search.set()
-    #await message.reply('Search text')
-
-async def input_search_text(message: types.Message):
-    #, state: FSMContext):
-    #async with state.proxy() as data:
-    #    data['q_search'] = message.text
+  
+async def input_search_spell(message: types.Message):
     print(message.text)
     search_text = " ".join(message.text.split()[1:])
-    #await FSMqueery_search.next()
-    await sqlite_db.sql_select_command(message, search_text)
-    #request=message.text.split()
-    #print(request)
-    #async with state.proxy() as data:
-        #data['q_search'] = message.text.split()
-        #print(data['q_search'])
-    #await message.reply(sqlite_db.sql_select_command(data['q_search']))
-    #await FSMqueery_search.next()
-    #async with state.proxy() as data:
-    #await FSMqueery_search.next()
-    #await message.reply('str(data)')
-    #await state.finish()
-    #for r in
-     #   await message.reply(sqlite_db.sql_select_command(" ".join(request[1:])))
+    await sqlite_db.sql_select_spell(message, search_text)
 
-    #await message.reply(message.text)
-    #await bot.send_message(message.from_user.id, message.text)
+
+async def input_search_base(message: types.Message):
+    print(message.text)
+    search_base = " ".join(message.text.split()[1:])
+    await sqlite_db.sql_select_base(message, search_base)
+
 
 def register_handlers(dp : Dispatcher):
-    dp.register_message_handler(command_start, commands=['start', 'help'])
-    #dp.register_message_handler(search_command, commands=['find'])#, state=None)
-    dp.register_message_handler(input_search_text, commands=['find'])
-    #state=FSMqueery_search.q_search)
+    dp.register_message_handler(command_start, commands=['start'])
+    dp.register_message_handler(command_help, commands=['help'])
+    dp.register_message_handler(input_search_spell, commands=['fs'])
+    dp.register_message_handler(input_search_base, commands=['fb'])
